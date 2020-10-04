8b08c3794e9180c9a80cc8decfca0ede5c46e681
David Currie <dac@10-18-232-213.dynapool.wireless.nyu.edu>
2020-10-03 20:08:06 -0400

    socketio

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+const options = {
+  /* ... */
+};
+const io = require('socket.io')(http, options);
+io.on('connection', socket => {
+  console.log(socket.id);
+});
 
+// routes
 app.use('/', express.static('public'));
 app.use('/overlay', express.static('overlay'));
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, process.env.IP, () => {
   console.log('listening on *:3000');
+});
+
+io.sockets.on('connection', socket => {
+  socket.on('task', data => {
+    socket.broadcast.emit('task', data);
+    console.log(data);
+  });
 });
--- overlay/sketch.js
+++ overlay/sketch.js
@@ -1,7 +1,26 @@
+let socket;
+let taskArray = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  socket = io.connect();
+  socket.on('task', receiveTask);
 }
 
 function draw() {
   background(220);
+  textSize(24);
+  for (let i = 0; i < taskArray.length; i++) {
+    text(taskArray[i], 0, 30 + i * 30);
+  }
+}
+
+function receiveTask({ task }) {
+  console.log('receiving task... I MUST: ', task);
+  taskArray.push(task);
+  console.log(taskArray);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
