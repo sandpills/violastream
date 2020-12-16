178f70b1fe72af2917e80cd0a5dedffcae92c9ea
viololahe <65051338+viololahe@users.noreply.github.com>
2020-12-15 22:07:31 -0500

    console.log

--- overlay/sketch.js
+++ overlay/sketch.js
@@ -30,6 +30,7 @@ function draw() {
     speaking = true;
       if ( speaking === true ) {
         textspeech.speak(tasks[i]); 
+        console.log (task[i]);
         speaking = false;
       }
   }
