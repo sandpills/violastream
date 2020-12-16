e6893483f21271d13ea0cac9c04a965f3a30906b
David Currie <davidalexandercurrie@gmail.com>
2020-12-16 01:35:33 -0500

    add delay timer and merge code from sketch/index.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,4 @@
-/**** THIS P5 SKETCH IS FOR THE TEXT FLY THING. EVENTUALLY IT SHOULD BE COMBINED INTO THE OVERLAY P5 */
-/** IT'S INCLUDED HERE BECAUSE I MADE THE TEXT INPUT BUTTONS ALSO IN P5 */
+socket = io.connect();
 
 let from;
 let to;
@@ -7,6 +6,10 @@ let xOff = 0.0;
 let w = [];
 let vid;
 let textspeech;
+let timerCountDown = 5; // VIOLA use this variable to make a p tag or something that shows above the buttons. This will show the timer value after a button is clicked
+let countDown;
+
+let buttonsActive = true;
 
 function setup() {
   from = 175;
@@ -14,69 +17,99 @@ function setup() {
 
   // the input
   input = createInput('tell me something');
-  input.parent("messagebox");
+  input.parent('messagebox');
   input.mousePressed(typing);
   input.position(0, 0);
   // the submit button
   button = createButton('submit');
-  button.parent("messagebox");
+  button.parent('messagebox');
   button.mousePressed(greet);
-  button.position(input.x + input.width+5, 0);
- 
+  button.position(input.x + input.width + 5, 0);
 
-//***everything else can be commented out****//
+  //***everything else can be commented out****//
 
-  var canvas = createCanvas(600,400);
-  canvas.parent("content");
-  canvas.position(0,0);
+  var canvas = createCanvas(600, 400);
+  canvas.parent('content');
+  canvas.position(0, 0);
   textAlign(CENTER);
   textSize(28);
-  
+
   textspeech = new p5.Speech();
 }
 
 function draw() {
   clear();
-  for (let i = 0; i < w.length; i++){
+  for (let i = 0; i < w.length; i++) {
     w[i].moveAndDisplay();
-    if (w[i].x <= -width){
+    if (w[i].x <= -width) {
       //w.splice(i, 1);
       w[i].x = null;
       w[i].y = null;
     }
   }
+  buttonsDelayTimer();
+}
+
+function buttonsDelayTimer() {
+  !buttonsActive
+    ? document.getElementById('commands').classList.add('buttons-inactive')
+    : document.getElementById('commands').classList.remove('buttons-inactive');
 }
 
 function typing() {
-    input.value('');
+  input.value('');
 }
 
 function greet() {
   const sentiment = input.value();
-  if (sentiment.length > 0){
-    let typedword = new Word(sentiment, width, random(10, height-10));
+  if (sentiment.length > 0) {
+    let typedword = new Word(sentiment, width, random(10, height - 10));
     w.push(typedword);
-    textspeech.speak(typedword.word); 
+    textspeech.speak(typedword.word);
     console.log(typedword);
     input.value('');
   }
 }
 
-class Word{
-  constructor(word, x, y){
+class Word {
+  constructor(word, x, y) {
     this.word = word;
     this.opacity = 255;
     this.x = x;
     this.y = y;
   }
 
-  moveAndDisplay(){
+  moveAndDisplay() {
     fill(255, this.opacity);
     textAlign(LEFT);
-    text(this.word, this.x+20, this.y-20)
-    textAlign(CENTER)
-    this.x-=3;
+    text(this.word, this.x + 20, this.y - 20);
+    textAlign(CENTER);
+    this.x -= 3;
     // this.opacity-=0.9;
     // console.log(this.x, this.y)
   }
+}
+
+function buttonClick(e) {
+  console.log(e.innerText); // any tag that's inside the HTML < > tag
+  sendTask(e.innerText);
+  buttonsActive = false;
+  countDown = setInterval(() => {
+    timerCountDown--;
+    console.log(timerCountDown);
+    if (timerCountDown == 0) {
+      clearInterval(countDown);
+    }
+  }, 1000);
+  setTimeout(() => {
+    buttonsActive = true;
+  }, 5000);
+}
+
+function sendTask(task) {
+  var data = {
+    task: task,
+  };
+
+  socket.emit('task', data);
 }
